287552da0e23e74794e516b62430d8a666edc846
XiaobingSuper <xiaobing.zhang@intel.com>
2023-01-17 20:52:43 -0500

    inductor: make as_strided support channels_last and always fix it's input layout
    ghstack-source-id: a414be28c69a3d17c77b1187daf6453b32d49c96
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92063

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -368,11 +368,16 @@ class GraphLowering(torch.fx.Interpreter):
                 result = super().run_node(n)
 
             # require the same stride order for dense outputs,
-            # so that user-land view() will not throw because inductor
+            # 1. user-land view() will not throw because inductor
             # output different strides than eager
             # long term the solution is to make view() always succeed
             # with infallible strides.
-            if any(user.op == "output" for user in n.users):
+            # 2: as_strided op, we need make sure its input has same size/stride with
+            # eager model to align with eager behavior.
+            if any(
+                user.op == "output" or user.target == torch.ops.aten.as_strided.default
+                for user in n.users
+            ):
                 strides = n.meta["val"].stride()
                 dense = torch._prims_common.is_non_overlapping_and_dense(n.meta["val"])
                 # requiring a stride order for a non-dense output wouldn't
